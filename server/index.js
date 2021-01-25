98b5c2016002aafbb7b949cddcffe81dfd0f208d
Roman Irinarhov <romairi1991@gmail.com>
2021-01-25 04:34:04 +0200

    Using Dependency Injection for a logger in the app

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {middleware} from './middleware';
 import {serverConfig} from "./config/serverConfig";
 import {connectDB} from "./services/connectDB";
+import Logger from "./services/logger";
 
 const app = express();
+const logger = new Logger();
 
 connectDB();
 
-middleware(app);
+middleware(app, logger);
 
-app.listen(serverConfig.port, () => console.log(`server listening on the port ${serverConfig.port}!`));
+app.listen(serverConfig.port, () => logger.log(`server listening on the port ${serverConfig.port}!`));
