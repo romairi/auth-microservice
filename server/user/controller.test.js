30a3315c641dae69cf6b229326ac309f036ac7a8
Roman Irinarhov <romairi1991@gmail.com>
2021-01-25 04:27:43 +0200

    Implementation unit test of loadUser function in controller

---
+++ server/user/controller.test.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import {loadUser} from "./controller";
+
+jest.mock('jsonwebtoken', () => ({
+    sign: jest.fn(() => 'TOKEN'),
+}));
+jest.mock('../config/serverConfig', () => ({
+    serverConfig: {jwt: {secret: "SECRET"}},
+}));
+
+describe('controller', () => {
+    describe('loadUser', () => {
+        const userMock = {
+            _id: 1,
+            toObject: () => ({password: 'PASSWORD', userName: 'USER_NAME'})
+        };
+
+        const resMock = {
+            json: jest.fn(),
+        };
+
+        it('should load the user', () => {
+            const result = loadUser(resMock, userMock);
+
+            expect(resMock.json.mock.calls.pop()).toMatchSnapshot();
+            expect(jwt.sign.mock.calls.pop()).toMatchSnapshot();
+            expect(result).toBe(resMock);
+        });
+    });
+});
