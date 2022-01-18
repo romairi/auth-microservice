5ab563565cdbca0f60e6b643dcf51cd18731e040
romairi <romairi1991@gmail.com>
2022-01-18 09:46:56 +0200

    init react project and install dependecies

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./containers/App/index";
+
+import "./index.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
