aa7ded90ddf3dbf4368fdc35ef264a47c623c07b
romairi <romairi1991@gmail.com>
2022-01-19 20:27:04 +0200

    comment languages

--- client/src/i18next.js
+++ client/src/i18next.js
@@ -1,26 +1,26 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import Backend from "i18next-xhr-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-import XHR from "i18next-xhr-backend";
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import Backend from "i18next-xhr-backend";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import XHR from "i18next-xhr-backend";
 
-const Languages = ["en", "ru", "fr"];
+// const Languages = ["en", "ru", "fr"];
 
-export default i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    // fallbackLng: "en",
-    fallbackLng: {
-      "en-US": ["en"],
-      "fr-FR": ["fr"],
-      "ru-RU": ["ru"],
-      default: ["en"],
-    },
-    debug: true,
-    // whitelist: Languages,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+// export default i18n
+//   .use(Backend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     // fallbackLng: "en",
+//     fallbackLng: {
+//       "en-US": ["en"],
+//       "fr-FR": ["fr"],
+//       "ru-RU": ["ru"],
+//       default: ["en"],
+//     },
+//     debug: true,
+//     // whitelist: Languages,
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//   });
