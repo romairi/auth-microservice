55c530ce767b214a3c31299ecaaced4c529ac896
romairi <romairi1991@gmail.com>
2022-01-30 16:59:38 +0200

    rename microservice

--- docker_test/webServer/server/config/serverConfig.js
+++ docker_test/webServer/server/config/serverConfig.js
@@ -4,5 +4,5 @@ import path from 'path';
 export const serverConfig = {
   port: envVar.PORT || 3030,
   staticFolder: path.join("../", "client", "build"),
-  microserviceLoginHost: envVar.MICROSERVICE_LOGIN_HOST  || 'http://localhost:8000',
+  microserviceAuthHost: envVar.MICROSERVICE_AUTH_HOST  || 'http://localhost:8000',
 };
