74536b1edbea62bd25447db2519c2b310bb3aa17
romairi <romairi1991@gmail.com>
2022-02-04 19:38:07 +0200

    connect redux for the root aplication

--- docker_test/webServer/client/src/index.js
+++ docker_test/webServer/client/src/index.js
@@ -1,15 +1,24 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App";
+import { Provider } from "react-redux";
 
+import App from "./containers/App";
+import store, {history} from "./redux/store";
+import { createSetUserAction } from "./redux/reducers/UserReducer/actions";
 import "./i18next";
 import "./index.scss";
 
+window.clientData = window.clientData || {};
+
+store.dispatch(createSetUserAction(window.clientData.user));
+
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App history={history}/>
+      </Suspense>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
